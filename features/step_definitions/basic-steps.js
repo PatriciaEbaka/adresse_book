48a05fad9308f768587cea96fd3c5908b02821dc
your_name <patricia.ebaka@gmail.com>
2019-01-30 22:12:30 +0100

    render contacts on the page

--- features/step_definitions/basic-steps.js
+++ features/step_definitions/basic-steps.js
@@ -24,10 +24,11 @@ Then('I fill in {string} with {string}', async function(string, string2) {
 
 Then('I should have {int} contact in my address book', async function(int) {
   // Write code here that turns the phrase above into concrete actions
-  return 'pending'
+  return await this.checkContactStorageCount(int)
 })
 
 Then('I should not see {string}', async function(string) {
   // Write code here that turns the phrase above into concrete actions
-  return 'pending'
+  return await this.pageDoesNotHaveTextContent(string)
+
 })
--- features/support/world.js
+++ features/support/world.js
@@ -53,6 +53,20 @@ class AddressBookWorld {
       break
     }
     }
+
+    async checkContactStorageCount(expectedCount) {
+        const actualCount = await this.page.evaluate(
+          () => JSON.parse(window.localStorage.getItem('contacts')).length
+        )
+        expect(actualCount).to.be.eq(expectedCount)
+      }
+
+      async pageDoesNotHaveTextContent(unexpectedContent) {
+        const pageContent = await this.page.content()
+        let actualContent = pageContent.match(unexpectedContent)
+    
+        expect(actualContent).to.be.eq(null)
+      }
 }
 
 setWorldConstructor(AddressBookWorld)
